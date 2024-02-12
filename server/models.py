eff5d765ee8fbbb543b54dbfae930e179c050d1c
Tim McCann <timothysmccann@gmail.com>
2024-02-12 12:06:05 -0500

    seeding db

--- server/models.py
+++ server/models.py
@@ -1,22 +1,25 @@
 from config import db
 
 class Trainer(db.Model):
+    __tablename__ = "trainer"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
     specialization = db.Column(db.String(255))
 
 class Pet(db.Model):
+    __tablename__ = "pet"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
     species = db.Column(db.String(255))
     temperament = db.Column(db.String(255))
+    muzzle = db.Column(db.String(255))
     trainer_id = db.Column(db.Integer, db.ForeignKey("trainer.id"))
     trainer = db.relationship("Trainer", backref=db.backref("pets", lazy="dynamic"))
 
 class Training(db.Model):
+    __tablename__ = "training"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
-    description = db.Column(db.String(255))
     trainer_id = db.Column(db.Integer, db.ForeignKey("trainer.id"))
     trainer = db.relationship("Trainer", backref=db.backref("trainings", lazy="dynamic"))
     pet_id = db.Column(db.Integer, db.ForeignKey("pet.id"))
--- server/seed.py
+++ server/seed.py
@@ -1,10 +1,58 @@
 from config import app
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models import *
+from models import db, Trainer, Pet, Training
 
 if __name__ == "__main__":
   with app.app_context():
-    
-    pass
-    # remove pass and write your seed data
+    Trainer.query.delete()
+    Pet.query.delete()
+    Training.query.delete()
+
+    trainer_1 = Trainer(
+      name="Pete",
+      specialization="Owner and head trainer"
+    )
+
+    db.session.add(trainer_1)
+
+    trainer_2 = Trainer(
+      name="Kayla",
+      specialization="Trainer and Groomer"
+    )
+
+    db.session.add(trainer_2)
+
+    trainer_3 = Trainer(
+      name="James",
+      specialization="Trainer"
+    )
+
+    db.session.add(trainer_3)
+
+    trainer_4 = Trainer(
+      name="John",
+      specialization="Groomer"
+    )
+  
+    db.session.add(trainer_4)
+
+    trainer_5 = Trainer(
+      name="Anthony",
+      specialization="Groomer"
+    )
+
+    db.session.add(trainer_5)
+
+    trainer_6 = Trainer(
+      name="Tim",
+      specialization="Aggressive dog training"
+    )
+
+    db.session.add(trainer_6)
+
+    trainer_7 = Trainer(
+      name="Jane",
+      specialization="Muzzle conditioning"
+    )
+
+    db.session.add(trainer_7)
+    db.session.commit()
