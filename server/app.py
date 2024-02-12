47d9eff23a6d418496b9084e5eb130d71732f797
Tim McCann <timothysmccann@gmail.com>
2024-02-12 10:25:43 -0500

    added some more to app file and got rich format to work

--- server/app.py
+++ server/app.py
@@ -2,8 +2,47 @@ from config import app, migrate
 from rich import print
 from models import db
 
+def display_welcome():
+  print("[cyan]Welcom to Fido Training and Stuff[/cyan]")
+    
+def display_main_menu():
+  print("[magenta]Main Menu[/magenta]")
+  print("1. Show all Trainers")
+  print("2. Add a new Trainer")
+  print("3. Show all Pets")
+  print("4. Add a new Pet")
+  print("5. Exit")
+
+def get_main_menu_choice():
+  return input("Which one would you like to do? ")
+
+def display_all_trainers():
+  pass
+
+def add_new_trainer():
+  pass
+
+def display_all_pets():
+  pass
+
+def add_new_pet():
+  pass
+
 if __name__ == "__main__":
   with app.app_context():
     migrate.init_app(app, db)
-    pass
-    # remove pass and write your cli logic
+    display_welcome()
+    while True:
+      display_main_menu()
+      choice = get_main_menu_choice()
+      if choice == "1":
+        display_all_trainers()
+      elif choice == "2":
+        add_new_trainer()
+      elif choice == "3":
+        display_all_pets()
+      elif choice == "4":
+        add_new_pet()
+      else:
+        break
+      break
